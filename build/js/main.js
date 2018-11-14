586e2a6bdfcb6ea160e6fddc3b88a2e20100a6b1
mazax1ct <vova@giveandget.ru>
2018-11-14 17:11:15 +0300

    catalog-section, search, brands

--- build/js/main.js
+++ build/js/main.js
@@ -63,4 +63,62 @@ $(document).ready(function() {
     $(".main-menu__link:not(.main-menu__link--root)").removeClass("hidden");
     $(".sub-menu").removeClass("is-open");
   });
+
+  //слайдер товаров
+  if ($('.js-products-slider').length) {
+    $('.js-products-slider').slick({
+      mobileFirst: true,
+      slidesToShow: 1,
+      infinite: true,
+      edgeFriction: 0,
+      arrows: false,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            arrows: true,
+            prevArrow: '<button type="button" class="slick-prev" title="Назад"><svg class="slick-prev__icon" aria-hidden="true"><use xlink:href="#slider_arrow_left"/></svg></button>',
+            nextArrow: '<button type="button" class="slick-next" title="Вперед"><svg class="slick-next__icon" aria-hidden="true"><use xlink:href="#slider_arrow_right"/></svg></button>'
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4
+          }
+        },
+        {
+          breakpoint: 479,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ]
+    });
+  }
+
+  //открытие фильтра каталога
+  $(".js-filter-opener").click(function() {
+    $(".filter__inner").toggleClass("is-open");
+  });
+
+  //открытие раздела фильтра каталога
+  $(".js-filter-section-opener").click(function() {
+    if($(this).hasClass("is-open")) {
+      $('.filter__inner').removeClass("section-open");
+      $('.filter__dropdown').removeClass("is-open");
+      $(".js-filter-section-opener").removeClass("is-open");
+    } else {
+      $(".js-filter-section-opener").removeClass("is-open");
+      $(this).addClass("is-open");
+      $('.filter__inner').addClass("section-open");
+      $('.filter__dropdown').removeClass("is-open");
+      $('.filter__dropdown[data-section='+ $(this).attr("data-section") +']').addClass("is-open");
+    }
+  });
 });
